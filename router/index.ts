7f2e82e35e6ac3ff1c71d414419713d9963881e6
piyoraik <42093052+piyoraik@users.noreply.github.com>
2021-09-14 20:47:32 +0900

    get /, post /

--- router/index.ts
+++ router/index.ts
@@ -1,15 +1,38 @@
-import { Router } from 'express'
+import { Router, Request } from 'express'
 
 const indexRouter = Router()
 
 let cnt = 0
 
+indexRouter.get('/', function (req, res) {
+  queryLog(req)
+  res.render('index.ejs', {
+    cnt,
+  })
+  cnt++
+})
+
+indexRouter.post('/', function (req, res) {
+  queryLog(req)
+  res.render('index.ejs', {
+    cnt,
+  })
+  cnt++
+})
+
 indexRouter.get('/page1', function (req, res) {
-  console.log(req.query.text_get)
   res.render('page1.ejs', {
     cnt,
   })
   cnt++
 })
+
+const queryLog = (req: Request) => {
+  if (req.method === 'POST') {
+    console.log('post_data: ' + req.body.name)
+  } else if (req.method === 'GET') {
+    console.log('get_data: ' + req.query.name)
+  }
+}
 
 export = indexRouter
